300f71081536d0659530e55cb60956363cb657bb
N.Pranav Krishna <52345192+npranav7619@users.noreply.github.com>
2023-04-12 15:11:25 +0530

    Update prereq.sh

--- prereq.sh
+++ prereq.sh
@@ -2,8 +2,10 @@
 echo "Enter pwd : "
 read pwd
 if (uname -v | grep "Arch") then
-  sudo pacman -S alacritty vim | pwd
+  sudo pacman -S alacritty | echo $pwd
  fi
 if (uname -v | grep "Void") then
-  sudo xbps-install alacritty | pwd
+  sudo xbps-install alacritty | echo $pwd
  fi
+#install agave fonts !
+#make alacritty config file and edit the font and colorscheme ! 
